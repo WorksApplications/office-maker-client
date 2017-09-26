850a5565fbf98a598c8ef05582b20c7ac458a644
torii_y <torii_y@worksap.co.jp>
2017-09-26 19:33:06 +0900

    make deploy script better

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,9 @@ cat config.json
 echo "okay? [yes/no]"
 
 read answer
-case $answer in
-    yes)
-        sh build.sh && node deploy
-        ;;
-    *)
-        echo "bye"
-        ;;
-esac
+if test "$answer" != "yes" ; then
+    echo "bye"
+    exit 1
+fi
+
+sh build.sh && node deploy
