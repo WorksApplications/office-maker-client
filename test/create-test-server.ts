2f66934381d753c95ba4ee03a596bd0e39ff8009
ishibashi_y <ishibashi_y@worksap.co.jp>
2019-05-23 19:52:32 +0900

    test: create mock server for back-end

--- test/create-test-server.ts
+++ test/create-test-server.ts
@@ -1,15 +1,13 @@
 // See ../watch.js
 import { Server } from 'http';
-import express from 'express';
+import express, { Request, Response } from 'express';
 import * as path from 'path';
 import * as child_process from 'child_process';
 
 const publicDir = path.resolve(__dirname, '../dest/public');
 const buildScriptPath = path.resolve(__dirname, '../build.sh');
 
-export const createServer: (port: number) => Promise<Server> = (
-  port: number
-) => {
+export const createElmServer = (port: number) => {
   const app = express();
   app.use((req, res, next) => {
     if (req.url.indexOf('/login') >= 0 || req.url.indexOf('/master') >= 0) {
@@ -21,9 +19,49 @@ export const createServer: (port: number) => Promise<Server> = (
   });
   app.use(express.static(publicDir));
 
-  return new Promise((resolve, reject) => {
-    child_process.execSync(`${buildScriptPath} test`, { shell: 'true' });
+  return new Promise<Server>((resolve, reject) => {
+    child_process.execSync(`sh ${buildScriptPath} test`);
 
+    const server = app.listen(port, () => {
+      resolve(server);
+    });
+  });
+};
+
+export const createMockServer = (
+  port: number,
+  apis: {
+    request: {
+      path: string;
+      method: 'POST' | 'GET' | 'PUT' | 'DELETE' | 'OPTIONS';
+    };
+    response: {
+      statusCode: number;
+      body?: any;
+      header?: { [key: string]: string };
+    };
+  }[],
+  middleware?: (req: Request, res: Response, next: () => void) => void
+) => {
+  const app = express();
+  if (middleware) {
+    app.use(middleware);
+  }
+
+  apis.forEach(api => {
+    app.use(api.request.path, (req, res, next) => {
+      if (req.method == api.request.method) {
+        res
+          .status(api.response.statusCode)
+          .set(api.response.header)
+          .send(api.response.body);
+      } else {
+        next();
+      }
+    });
+  });
+
+  return new Promise<Server>((resolve, reject) => {
     const server = app.listen(port, () => {
       resolve(server);
     });
--- test/floor.spec.ts
+++ test/floor.spec.ts
@@ -1,4 +1,4 @@
-import { createServer } from './create-test-server';
+import { createElmServer, createMockServer } from './create-test-server';
 import { Server } from 'http';
 
 declare global {
@@ -13,20 +13,55 @@ const { toMatchImageSnapshot } = require('jest-image-snapshot');
 expect.extend({ toMatchImageSnapshot });
 
 let app: Server;
+let mock: Server;
 
 beforeAll(async () => {
-  app = await createServer(30300);
-  await page.setCacheEnabled(false);
+  app = await createElmServer(30300);
+  mock = await createMockServer(
+    30400,
+    [
+      {
+        request: {
+          path: '/api/floors',
+          method: 'GET'
+        },
+        response: {
+          statusCode: 200,
+          body: []
+        }
+      }
+    ],
+    (req, res, next) => {
+      // CORS Settings
+      res.header('Content-Type', 'application/json');
+      res.header('access-control-allow-credentials', 'true');
+      res.header('access-control-allow-origin', '*');
+
+      if (req.method == 'OPTIONS') {
+        res.header(
+          'access-control-allow-headers',
+          'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent,Pragma,Cache-Control,If-Modified-Since'
+        );
+        res.header('access-control-allow-methods', 'OPTIONS,GET');
+        res.sendStatus(200);
+      } else {
+        next();
+      }
+    }
+  );
 });
 
 afterAll(async () => {
   app.close();
+  mock.close();
 });
 
 describe('Open floor page', () => {
   beforeEach(async () => {
     await page.goto('http://localhost:30300');
-    await page.waitForNavigation();
+
+    // why waitForNavigation does not work? (keep waiting forever)
+    await page.waitFor(2000);
   });
 
   it('should match the image snapshot', async () => {
